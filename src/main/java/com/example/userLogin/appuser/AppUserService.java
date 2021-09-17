ad742df0aa7e8f6f31297aca395fd7ddc39ea6b8
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2021-09-16 22:49:13 -0300

    Create: classes to registration

--- src/main/java/com/example/userLogin/appuser/AppUserService.java
+++ src/main/java/com/example/userLogin/appuser/AppUserService.java
@@ -1,6 +1,25 @@
 package com.example.userLogin.appuser;
 
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor //faz com que nao precise criar os constructor
+//especifica para spring security
 public class AppUserService implements UserDetailsService {
+
+    private final static String USER_NOT_FOUND = "USER WITH EMAIL %S NOT FOUND";
+    private final AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                String.format(USER_NOT_FOUND, email)));
+    }
 }
---
+++ src/main/java/com/example/userLogin/registration/RegistrationService.java
@@ -0,0 +1,8 @@
+package com.example.userLogin.registration;
+
+public class RegistrationService {
+
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
